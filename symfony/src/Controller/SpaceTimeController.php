c5f4e0e5983a7a6dd82ddf1ca9cfa81c3a1c0384
Dennis <den.peld@gmail.com>
2020-05-22 13:54:55 +0200

    Refactoring of a main controller

--- symfony/src/Controller/SpaceTimeController.php
+++ symfony/src/Controller/SpaceTimeController.php
@@ -6,7 +6,10 @@ namespace App\Controller;
 
 use App\EarthTime\UTC;
 use App\MarsTime\MartianDateTimeConverter;
+use App\MarsTime\MartianDateTimeFormatter;
 use App\SpaceTime\Converter;
+use App\SpaceTime\Formatter;
+use App\SpaceTime\SpaceTimeError;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -19,33 +22,32 @@ class SpaceTimeController extends AbstractController
     const STATUS_CODE_OK = 200;
     const STATUS_CODE_BAD_REQUEST = 400;
 
+    private $responseHeader = ['Content-Type' => 'application/json'];
+
     /**
      * @Route("/convert/{earthtime}", name="spacetime_convert")
      */
     public function convert(string $earthtime = null): Response
     {
         try {
-            $utc = new UTC();
-            $converter = new Converter(new MartianDateTimeConverter(), $utc->build($earthtime));
+            $converter = new Converter(
+                new MartianDateTimeConverter(UTC::getUTC($earthtime))
+            );
 
-            $data =  [
-                'mars_sol_date' => $converter->getDate(),
-                'martian_coordinated_time' => $converter->getTime(),
-            ];
+            $formatter = new Formatter(
+                new MartianDateTimeFormatter($converter)
+            );
             
             $response = new Response(
-                json_encode($data), 
+                $formatter->getFormattedOutputAsJsonString(), 
                 self::STATUS_CODE_OK, 
-                ['Content-Type' => 'application/json']
+                $this->responseHeader
             );
         } catch(\Exception $e) {
-            $data =  [
-                'error' => $e->getMessage(),
-            ];
             $response = new Response(
-                json_encode($data),
+                SpaceTimeError::getErrorOutputAsJsonString($e),
                 self::STATUS_CODE_BAD_REQUEST, 
-                ['Content-Type' => 'application/json']
+                $this->responseHeader
             );
         }
         
