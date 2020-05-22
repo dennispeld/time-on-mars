5ccf0a246a4b85c61c7b63e743cb9765cb66084b
Dennis <den.peld@gmail.com>
2020-05-22 17:40:42 +0200

    Added MartianDateTimeFormatterTest class

---
+++ symfony/tests/MarsTime/MartianDateTimeFormatterTest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Tests\MarsTime;
+
+use App\EarthTime\UTC;
+use App\MarsTime\MartianDateTimeConverter;
+use App\MarsTime\MartianDateTimeFormatter;
+use App\SpaceTime\ConverterFactory;
+use PHPUnit\Framework\TestCase;
+
+class MartianDateTimeFormatterTest extends TestCase
+{
+    /**
+     * Test if formatter retrieves the expected data in Json string
+     */
+    public function testOutputFormatAsJsonString()
+    {
+        $timestamp = '1590141800';
+        $utc = UTC::getUTC($timestamp);
+        $converter = new MartianDateTimeConverter($utc);
+        $converterFactory = new ConverterFactory($converter);
+        $formatter = new MartianDateTimeFormatter($converterFactory);
+        $actualOutput = $formatter->getFormattedOutputAsJsonString();
+        $this->assertJson($actualOutput);
+        $this->assertStringContainsString('mars_sol_date', $actualOutput);
+        $this->assertStringContainsString('martian_coordinated_time', $actualOutput);
+    }
+}
