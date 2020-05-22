3a98863c7e25dc52de9cdaf2788a818d09948588
Dennis <den.peld@gmail.com>
2020-05-22 13:40:16 +0200

    Added support of timestamps

--- symfony/src/EarthTime/UTC.php
+++ symfony/src/EarthTime/UTC.php
@@ -4,17 +4,44 @@ declare(strict_types = 1);
 
 namespace App\EarthTime;
 
+use DateTimeImmutable;
+use Exception;
+
 class UTC
-{
-    public function build(string $dateTimeOnEarth = null): \DateTime
+{   
+    public static function getUTC(string $dateTimeOnEarth = null): \DateTime
     {
+        // if no datetime was specified, set it to current datetime
         if (!$dateTimeOnEarth) {
             $dateTimeOnEarth = 'now';
         }
 
-        $date = new \DateTime($dateTimeOnEarth);
-        $date->setTimezone(new \DateTimeZone('UTC'));
+        try {
+            if (!self::isTimestamp($dateTimeOnEarth)) {
+                // accept date in certain formats (for example, 24.06.1983 or 24-06-1983)
+                $utc = new \DateTime($dateTimeOnEarth);
+            } else {
+                // accept UNIX timestamps (for eaxmple, 1590141800)
+                $utc = new \DateTime();
+                $utc->setTimestamp((int) $dateTimeOnEarth);
+            }
+            
+            $utc->setTimezone(new \DateTimeZone('UTC'));
+        } catch(\Exception $e) {
+            throw new Exception('The datetime format is wrong.');
+        }
 
-        return $date;
+        return $utc;
     }
+
+    /**
+     * Check if the given string is a valid timestamp
+     */
+    private static function isTimestamp(string $dateTimeOnEarth)
+    {
+        return ((string) (int) $dateTimeOnEarth === $dateTimeOnEarth) 
+            && ($dateTimeOnEarth <= PHP_INT_MAX)
+            && ($dateTimeOnEarth >= ~PHP_INT_MAX);
+    }
+    
 }
